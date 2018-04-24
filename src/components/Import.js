fb7577cc45d04b5808537397ab23ed360d256d42
Urs Forrer <mail@ursforrer.ch>
2018-04-24 14:50:13 +0200

    Add further descriptions
    Add further descriptions

--- src/components/Import.js
+++ src/components/Import.js
@@ -53,9 +53,14 @@ class Import extends Component {
           <title>Tourlive Admin | Import</title>
         </Helmet>
         <Header as="h1" color='red'>Import</Header>
+        <Header as="h3">Import der initalen Daten von der cnlab API</Header>
+        <p>Dieser Schritt muss nur zu Beginn einer Verantstaltung (z.B. Tour de Suisse) durchgeführt werden.</p>
         {loadingCycle}
         <Button primary fluid onClick={this.initialImport}>Initale Daten von der cnlab API holen</Button>
-        <br/><Divider /><br/>
+        <br/><Divider />
+        <Header as="h3">Import der aktuellen Daten vom Zeitnehmer Matsport (pro Etappe)</Header>
+        <p>Nach jedem Rennen stehen vom offiziellen Zeitnehmer die aktuellen (korrekten) Daten zur Verfügung. Diese Daten werden mit diesem Schritt eingelesen.</p>
+        <p>Dabei werden die Daten der vergangenen Etappe überschrieben. Zudem wir die nächste Etappe mit diesen Daten vorbefüllt.</p>
         <Form>
           <Message
             success
@@ -67,7 +72,24 @@ class Import extends Component {
             header='Speichern fehlgeschlagen'
             content='Das Speichern der Einstellungen ist fehlgeschlagen. Versuchen Sie es später erneut und prüfen Sie die Umsysteme (API) auf Fehler.'
           />
-          <Button primary fluid>XML hochladen und in API importieren</Button>
+          <Button primary fluid>Matsport XML hochladen und in API importieren</Button>
+        </Form>
+        <br/><Divider />
+        <Header as="h3">Import der GPX Daten (pro Etappe)</Header>
+        <p>Nach jedem Rennen stehen vom offiziellen Zeitnehmer die aktuellen (korrekten) Daten zur Verfügung. Diese Daten werden mit diesem Schritt eingelesen.</p>
+        <p>Die Aktualisierung der GPX Daten kann zu jederzeit erfolgen. Es empfiehlt sich die Daten bereits zu Beginn des Rennens zu aktualisieren.</p>
+        <Form>
+            <Message
+                success
+                header='Einstellungen gespeichert'
+                content="Die Einstellungen wurden erfolgreich gespeichert. "
+            />
+            <Message
+                error
+                header='Speichern fehlgeschlagen'
+                content='Das Speichern der Einstellungen ist fehlgeschlagen. Versuchen Sie es später erneut und prüfen Sie die Umsysteme (API) auf Fehler.'
+            />
+            <Button primary fluid>GPX XML hochladen und in API importieren</Button>
         </Form>
       </div>
 
